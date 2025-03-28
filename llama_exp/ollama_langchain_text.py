92c5b7de3d451c04c7db656b482082e5ada8d428
asya orlova <asya-1610@yandex.ru>
2025-03-28 12:51:30 +0300

    add rebuttal

---
+++ llama_exp/ollama_langchain_text.py
@@ -0,0 +1,156 @@
+from langchain_core.prompts import ChatPromptTemplate, FewShotPromptTemplate, PromptTemplate
+from langchain_ollama.llms import OllamaLLM
+# from langchain.chains import FewShotChain
+import pandas as pd
+from tqdm import tqdm
+from sys import argv
+
+dataset_path = argv[1]
+column = argv[2]
+mod = argv[3]
+save_dataset_path = argv[4]
+
+df = pd.read_csv(dataset_path)
+
+df['high_yielding'] = df['high_yielding'].apply(lambda x: 'High-yielding' if x==1 else 'Not high-yielding')
+
+df_train = pd.DataFrame(df[df.split=='train'])
+df_test = pd.DataFrame(df[df.split=='test'])
+
+
+zero_shot_template = {
+    "smiles": "You are an expert chemist. Your task is to predict reaction yields based on SMILES representations"
+              " of organic reactions. Reaction SMILES consist of potentially three parts (reactants, agents,"
+              " and products) each separated by an arrow symbol '>'. Reactants are listed before the arrow symbol."
+              " If a reaction includes agents, such as catalysts or solvents, they can be included after the reactants."
+              " Products are listed after the second arrow symbol, representing the resulting substances of the"
+              " reaction. You can only predict whether the reaction is 'High-yielding' or 'Not high-yielding'."
+              " 'High-yielding' reaction means the yield rate of the reaction is above 70%. 'Not high-yielding'"
+              " means the yield rate of the reaction is below 70%. You will be provided with several examples of"
+              " reactions and corresponding yield rates. Strictly answer with only 'High-yielding' or"
+              " 'Not high-yielding', no other information can be provided.",
+    "text": "You are an expert chemist. Based on text descriptions of organic reactions"
+            " you predict their yields using your experienced reaction yield prediction knowledge."
+            " You can only predict whether the reaction is 'High-yielding' or 'Not high-yielding'."
+            " 'High-yielding' reaction means the yield rate of the reaction is above 70%."
+            " 'Not high-yielding' means the yield rate of the reaction is below 70%."
+            " You will be provided with several examples of reactions and corresponding yield rates."
+            " Please answer with only 'High-yielding' or 'Not high-yielding', no other information can be provided."}
+
+# k = 6
+# seed = 36
+system_prompt = zero_shot_template['text']
+model = OllamaLLM(model=mod, temperature=0.1, max_predict=50)
+example_prompt = PromptTemplate.from_template("Reaction: {question}\nAnswer: {answer}")
+instruction = zero_shot_template['text']
+
+# prompt_template = PromptTemplate(input_variables=['examples', 'reaction'],
+#                                  template="You are an expert chemist. Based on text descriptions of organic reactions"
+#                                  " you predict their yields using your experienced reaction yield prediction knowledge."
+#                                   " You can only predict whether the reaction is 'High-yielding' or 'Not high-yielding'."
+#                                   " 'High-yielding' reaction means the yield rate of the reaction is above 70%."
+#                                   " 'Not high-yielding' means the yield rate of the reaction is below 70%."
+#                                   " You will be provided with several examples of reactions and corresponding yield rates."
+#                                   " Please answer with only 'High-yielding' or 'Not high-yielding', no other information can be provided.\n"
+#                                   "{examples}\Reaction: {reaction}"
+#                                  )
+
+
+def equal_sampler(seed, k):
+  
+  samples = []
+  
+  for idx in df_train.index:
+    d = dict()
+    if not samples:
+      d['question'] = df_train.loc[idx, column]
+      d['answer'] = df_train.loc[idx, 'high_yielding']
+      samples.append(d)
+      df_train.drop([idx], inplace=True)
+    else:
+      if df_train.loc[idx, 'high_yielding'] not in samples[0].values():
+        d['question'] = df_train.loc[idx, column]
+        d['answer'] = df_train.loc[idx, 'high_yielding']
+        samples.append(d)
+        df_train.drop([idx], inplace=True)
+        break
+   
+  df_choice = df_train.sample(k-2, random_state=seed)
+  for idx in df_choice.index:
+    d = dict()
+    d['question'] = df_train.loc[idx, column]
+    d['answer'] = df_train.loc[idx, 'high_yielding']
+    samples.append(d)
+  
+  return samples
+
+# def equal_sampler(seed, k):
+  
+#   samples = []
+  
+#   for idx in df_train.index:
+#     d = dict()
+#     if not samples:
+#       d['question'] = df_train.loc[idx, column]
+#       d['answer'] = df_train.loc[idx, 'high_yielding']
+#       samples.append(d)
+#       df_train.drop([idx], inplace=True)
+#     else:
+#       if df_train.loc[idx, 'high_yielding'] not in samples[0].values():
+#         d['question'] = df_train.loc[idx, column]
+#         d['answer'] = df_train.loc[idx, 'high_yielding']
+#         samples.append(d)
+#         df_train.drop([idx], inplace=True)
+#         break
+   
+#   df_choice = df_train.sample(k-2, random_state=seed)
+#   for idx in df_choice.index:
+#     d = dict()
+#     d['question'] = df_train.loc[idx, column]
+#     d['answer'] = df_train.loc[idx, 'high_yielding']
+#     samples.append(d)
+  
+#   new_samples = []
+#   for i in samples:
+#     text = f"Reaction: {i['question']}\nAnswer: {i['answer']}"
+#     new_samples.append(text)
+  
+#   return new_samples
+
+# def get_response(system_prompt, samples, reaction, model, seed):
+#   messages = [{"role": "system", "content": system_prompt}]
+#   for s, y in samples.items():
+#     y = 'high_yielding' if y==1 else 'not_high_yielding'
+#     messages.append(
+#                 {"role": "user", "content": s}
+#             )
+#     messages.append({"role": "assistant", "content": y})  
+#   messages.append({"role": "user", "content": reaction}) 
+#   response: ChatResponse = chat(model=model, messages=messages, options={'seed':seed})
+#   return response['message']['content']
+
+
+for k in tqdm([4]):
+  for seed in [36, 42, 84, 200, 12345]:
+    for idx in tqdm(df_test.index):
+        reaction = df_test.loc[idx, column]
+        # print(seed, k)
+        samples = equal_sampler(seed, k)
+        # few_shot_chain = FewShotChain(llm=model, prompt=prompt_template, support=samples)
+        # for i in samples:
+        #   print(i)
+        #   print('\n')
+        prompt = FewShotPromptTemplate(examples=samples, example_prompt=example_prompt, suffix="Reaction: {input}\nAnswer:", input_variables=["input"], prefix=instruction)
+        # print(prompt)
+        # final_prompt = prompt.invoke({"input": reaction}).to_string()
+        # print(final_prompt)
+        chain = prompt | model
+        out = chain.invoke({"input": reaction})
+        # out = few_shot_chain.run(query=reaction)
+        print(out.split('</think>')[1])
+        df_test.loc[idx, f'response_{k}_{seed}'] = out.split('</think>')[1]
+    df_test.to_csv(f'{save_dataset_path}', index=False)
+  df_test.to_csv(f'{save_dataset_path}', index=False)
+
+df_test.to_csv(f'{save_dataset_path}', index=False)
+  
---
+++ rebuttal/README.md
@@ -0,0 +1,53 @@
+# Rebuttal
+
+Here we provide the results of additional experiments requested by reviewers.
+
+## How well do LLMs perform in predicting precise yield values?
+
+In addition to our regression experiments provided in Appendix A.1, we tested LLMs performance in predicting precise yield values on the USPTO-R dataset across two settings:
+
+1) We selected Claude 3 Haiku as one of the best performing models and asked it to predict reaction yields in a few-shot approach with the following system prompt:
+
+*You are an expert chemist. Based on text descriptions of organic reactions your task is to predict their yields using your experienced reaction yield prediction knowledge. You will be provided with several examples of reactions and corresponding yields measured in %. Based on these examples, predict the yield of the given reaction. Please, answer with only one float number, do not write anything else.*
+
+2) We selected Mistral 7B embeddings as the best performing ones for the USPTO-R dataset and employed grid-serach to find the optimal hyperparameters of the XGBRegressor model.
+
+The comparison of obtained results with baselines is provided below:
+
+|                             |           USPTO-R           |
+|-----------------------------|--------------|--------------|
+|                             |      R2      |     RMSE     |
+|-----------------------------|--------------|--------------|
+| Yield-BERT                  | -0.03        | 25.91        |
+| T5Chem                      | -15.53       | 103.47       |
+| Egret                       | -0.16        | 27.51        |
+| DRFP + XGB                  | -0.03 ± 0.01 | 25.90 ± 0.16 |
+|-----------------------------|--------------|--------------|
+| Claude 3 Haiku (k=2)        | -0.14 ± 0.14 | 27.22 ± 1.64 |
+| Claude 3 Haiku (k=4)        | -0.13 ± 0.13 | 27.11 ± 1.46 |
+| Claude 3 Haiku (k=6)        | -0.10 ± 0.08 | 26.82 ± 0.91 |
+| Claude 3 Haiku (k=8)        | -0.07 ± 0.08 | 26.37 ± 1.04 |
+| Claude 3 Haiku (k=10)       | -0.10 ± 0.08 | 26.80 ± 0.94 |
+| Mistral 7B embeddings + XGB |  **0.06 ± 0.00** | **24.71 ± 0.03** |
+
+**Conclusion:**
+
+Since the regression problem is challenging, LLMs do not provide impressive performance similarly to baselines.
+However, we would like to highlight that Mistral 7B embeddings achieve best performance among other approaches, which again emphasizes the ability of LLM embeddings to become novel SOTA reaction representations.
+
+## How chemical LLMs compare to generalist LLMs?
+
+To compare chemical LLMs directly with generalist LLMs in the few-shot setting, we conducted additional **few-shot experiments with chemical LLMs** on the USPTO-R dataset. Despite changes in prompts and model configuration, Galactica-6.7B consistently responded with the same yield category, producing the same metrcis across all k values.
+The results are provided below:
+
+|                  |        2        |                 |        4        |                 |        6        |                 |        8        |                 |        10       |                 |
+|------------------|:---------------:|:---------------:|:---------------:|:---------------:|:---------------:|:---------------:|:---------------:|:---------------:|:---------------:|:---------------:|
+|                  | Accuracy        | F1-score        | Accuracy        | F1-score        | Accuracy        | F1-score        | Accuracy        | F1-score        | Accuracy        | F1-score        |
+| ChemDFM-v1.0-13B |   0.47 ± 0.05   |   0.41 ± 0.09   |   0.44 ± 0.04   |   0.41 ± 0.18   |   0.49 ± 0.03   |   0.57 ± 0.09   |   0.45 ± 0.05   |   0.43 ± 0.12   |   0.50 ± 0.05   |   0.45 ± 0.23   |
+| Galactica-6.7B   |   0.50 ± 0.00   |   0.67 ± 0.00   |   0.50 ± 0.00   | **0.67 ± 0.00** |   0.50 ± 0.00   | **0.67 ± 0.00** |   0.50 ± 0.00   |   0.67 ± 0.00   |   0.50 ± 0.00   | **0.67 ± 0.00** |
+| ChemLLM-7B-Chat  |   0.33 ± 0.01   |   0.49 ± 0.01   |   0.32 ± 0.02   |   0.49 ± 0.02   |   0.35 ± 0.03   |   0.52 ± 0.03   |    0.4 ± 0.03   |   0.57 ± 0.03   |   0.44 ± 0.03   |   0.60 ± 0.03   |
+| Claude 3 Haiku   | **0.52 ± 0.00** | **0.68 ± 0.00** |   0.57 ± 0.02   |   0.59 ± 0.04   |   0.55 ± 0.02   |   0.67 ± 0.02   |   0.54 ± 0.03   | **0.68 ± 0.01** |   0.53 ± 0.01   |   0.65 ± 0.03   |
+| Mistral Small    |   0.51 ± 0.00   |   0.66 ± 0.00   | **0.59 ± 0.03** |   0.60 ± 0.04   | **0.61 ± 0.02** |   0.63 ± 0.04   | **0.56 ± 0.03** |   0.62 ± 0.02   | **0.57 ± 0.03** |   0.61 ± 0.05   |
+
+Conclusion:
+I can be observed that in the few-shot setting generalist LLMs still outperform chemical LLMs in most cases. The best accuracy=0.61 achieved by Mistral Small (k=6) is 11% higher than the best result achieved by chemical LLMs (accuracy=0.5 by Galactica-6.7B).
